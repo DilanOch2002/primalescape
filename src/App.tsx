a2fcd8eb767d4584529aab2b7ea588ad2499ae6d
DilanOch2002 <106693124+DilanOch2002@users.noreply.github.com>
2025-11-17 22:29:44 -0500

    aaa v2

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
-import BigCarousel from "./components/BigCarousel/BigCarousel";
 import FeatureGrid from "./components/FeatureGrid/FeatureGrid";
+import BigCarousel from "./components/BigCarousel/BigCarousel";
 import Footer from "./components/Footer/Footer";
 
-
 function App() {
-return (
-<>
-<Navbar />
-<Hero />
-<FeatureGrid />
-<BigCarousel />
-<Footer />
-</>
-);
+  return (
+    <>
+      <Navbar />
+      <main>
+        <Hero />
+        <FeatureGrid />
+        <BigCarousel />
+      </main>
+      <Footer />
+    </>
+  );
 }
-
 
 export default App;
--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -1,12 +1,15 @@
-import './MainMenu.css';
+import "./MainMenu.css";
 
 const MainMenu = () => {
   return (
     <div className="menu-container">
       <h1 className="title">PRIMAL ESCAPE</h1>
-      <button className="btn">Start Game</button>
-      <button className="btn">Options</button>
-      <button className="btn">Exit</button>
+
+      <div className="btn-group">
+        <button className="btn">Start Game</button>
+        <button className="btn">Options</button>
+        <button className="btn">Exit</button>
+      </div>
     </div>
   );
 };
